376caa1e379abf40af7d314d335911da775c46e2
peacefulavocado <54719329+PeacefulAvocado@user.noreply.github.com>
2022-11-01 20:22:46 +0100

    vlatozok tombok kesz

--- script.js
+++ script.js
@@ -19,10 +19,27 @@ function closeMenu() {
 }
 
 function copyClipboard(id){
-    if (id == "") {}
+    var copyText = "";
+    const sinppets = [
+        ["codesnippet-a",'string name = "Jhon";'],
+        ["codesnippet-b",'int[] array = new int[n];'],
+        ["codesnippet-c",'int[] array;\narray = new int[n];'],
+        ["codesnippet-d",'int[] array = new int[5] { 1, 2, 3, 4, 5 };'],
+        ["codesnippet-e",'int[] array = { 1, 2, 3, 4, 5 };'],
+        ["codesnippet-f",'int[] array = { 1, 2, 3, 4, 5 };\nConsole.WriteLine(array[10]);'],
+        ["codesnippet-g",'for (int i = 0; i < n; i++)\n{\n\t//ciklusmag\n}'],
+        ["codesnippet-h",'int[] array = { 1, 2, 3, 4, 5 };\nreturn array.Length;'],
+        ["codesnippet-i",'int[] array = { 1, 2, 3, 4, 5 };\nreturn array[2];']
 
-    var Text = document.getElementById(id)
-    navigator.clipboard.writeText(Text.ariaValueText);
+    ]
+    let i = 0    
+    while (i<sinppets.length && sinppets[i][0]!=id) {
+        i++
+        console.log(sinppets[i])
+    }
+    copyText = sinppets[i][1]
+    
+    navigator.clipboard.writeText(copyText);
 }
 
 function submit() {
