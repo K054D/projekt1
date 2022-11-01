184ec3e2a3c3d3da9dcf61f5e425b7c7648a7ed1
tothjonas <54719329+PeacefulAvocado@users.noreply.github.com>
2022-11-01 15:19:40 +0100

    test-prevv
    irjatok kerdeseket :)

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ function copyClipboard(id){
     var Text = document.getElementById(id)
     navigator.clipboard.writeText(Text.ariaValueText);
 }
+
+function submit() {
+    var count = 0
+    var x = Array.from(document.getElementsByClassName("correct"))
+    const r = document.getElementById("reset")
+    const s = document.getElementById("submit");
+    x.forEach(element => {
+        if(element.checked)
+            count++
+    });
+    document.getElementById("ered").innerHTML="Eredmény: "+count.toString()+"/3"
+    r.style.display = "block";
+    s.style.display = "none";
+    } 
+
+
+function reset() {
+    const r = document.getElementById("reset");
+    const s = document.getElementById("submit");
+    document.getElementById("ered").innerHTML="";
+    r.style.display = "none";
+    s.style.display = "block";
+    for (let i = 1; i < 10; i++) {
+        try {
+            document.querySelector(`input[name=select${i.toString()}]:checked`).checked = false;
+        } catch (error) {
+            //pass
+        }
+        
+    }
+    }
